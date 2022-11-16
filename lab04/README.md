4aed74a619bd8875241c45b09771372fef821d32
kvuchkov <kirilvuchkov@gmail.com>
2022-11-16 10:21:56 +0200

    Lab 04

---
+++ lab04/README.md
@@ -0,0 +1,18 @@
+# Control flow (if)
+
+## Setup
+We can leverage the IDE (VS Code) to compile and run our program with a shortcut. To do this, you need to setup the `.vscode/tasks.json` with two tasks: one for compiling the source (active file) and the other to run it.
+
+Look at the `.vscode/tasks.json` file in the root of this repository.
+
+You can then compile the currently open file with `CTRL+SHIFT+B` on Windows/Linux and `CMD+SHIFT+B` on Mac and run it with `CTRL+SHIFT+T` on Windows/Linux and `CMD+SHIFT+T` on Mac.
+
+To check what/if you have a shortcut, hit `CTRL+SHIFT+P` (or `CMD+SHIFT+P` on Mac) to open the command palette and write "run test" (see screenshot). You can configure a shortcut by pressing the gear icon next to the command. 
+
+## Change
+Write a program for a vending machine that calculates how much is the change and what coins to use to return it based on the total amount due and amount paid by the customer. The program should work with integers (i.e. using the cents value of money).
+
+1) Create your source file as `change.cpp`
+2) Write the code
+3) Compile often with `CTRL+SHIFT+B` (or `CMD+SHIFT+B`)
+4) Run often with `CTRL+SHIFT+T` (or `CMD+SHIFT+T`)
---
+++ lab04/change.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int total, paid;
+    
+    cin >> total >> paid;
+
+    int change = paid - total;
+    if (change < 0) {
+        cout << "The total exceeds the amount paid." << endl;
+        return 1;
+    }
+
+    int left = change;
+
+    int count2lv = left / 200;
+    if (count2lv > 0) {
+        cout << count2lv << "x " << 2.00 << " lv." << endl;
+        left = left % 200;
+    }
+    
+    int count1lv = left / 100;
+    if (count1lv > 0) {
+        cout << count1lv << "x " << 1.00 << " lv." << endl;
+        left = left % 100;
+    }
+
+    int count50st = left / 50;
+    if (count50st > 0) {
+        cout << count50st << "x " << 50 << " st." << endl;
+        left = left % 50;
+    }
+
+    int count20st = left / 20;
+    if (count20st > 0) {
+        cout << count20st << "x " << 20 << " st." << endl;
+        left = left % 20;
+    }
+
+    int count10st = left / 10;
+    if (count10st > 0) {
+        cout << count10st << "x " << 10 << " st." << endl;
+        left = left % 10;
+    }
+
+    int count5st = left / 5;
+    if (count5st > 0) {
+        cout << count5st << "x " << 5 << " st." << endl;
+        left = left % 5;
+    }
+
+     int count2st = left / 2;
+    if (count2st > 0) {
+        cout << count2st << "x " << 2 << " st." << endl;
+        left = left % 2;
+    }
+
+    if (left) cout << left << "x " << 1 << "st." << endl;
+
+    return 0;
+}
